ad158d49ab039f5d878a8ee051003daecfc3bbb2
pervanne69 <samvel.pogosyan2006@gmail.com>
2023-10-31 09:15:01 +0300

    some files

--- running_string/main.c
+++ running_string/main.c
@@ -1,3 +1,30 @@
-//
-// Created by acer on 29.10.2023.
-//
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, const char* argv[])
+{
+    const char text[] = "Hello World";
+
+    size_t i = 0, j;
+
+    while (1)
+    {
+        for (j = i; j > 0; --j)
+        {
+            printf(" ");
+        }
+
+        printf("%.*s", 80-i-1, text);
+
+        if (80 == ++i)
+        {
+            i = 0;
+        }
+
+        printf("%c", 13);
+
+        usleep(100000);
+    }
+
+    return 0;
+}
