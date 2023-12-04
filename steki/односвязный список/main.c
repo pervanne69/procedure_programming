1b53371f86212dbfd8bc7e55a4e8e969eae8e71f
pervanne69 <samvel.pogosyan2006@gmail.com>
2023-12-04 11:55:22 +0300

    adding_list_init

---
+++ steki/односвязный список/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef int Data;
+
+typedef struct Node Node;
+
+struct Node {
+    Data data;
+    Node * next;
+};
+
+int main() {
+    struct Node * list = NULL;
+
+    return 0;
+}
